c4ba887fea78c4208cf97c35bf8451337af7f1a5
Erin Chon <echon@bsd220.org>
2022-11-07 10:04:48 -0600

    added dice images

--- roll the die hackwich/ContentView.swift
+++ roll the die hackwich/ContentView.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var randomValue = 0
+    @State private var rotation = 0.0
     var body: some View {
         VStack {
             Text("Dice Roll").font(.title)
             Text("\(randomValue)")
                 .font(.system(size: 72))
                 .padding()
+            Image("pips \(randomValue)")
+                .resizable()
+                .frame(width: 200, height: 200, alignment: .center)
+                .rotationEffect(.degrees(rotation))
+                .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
                 .onTapGesture {
                     randomValue = Int.random(in:1...6)
                 }
