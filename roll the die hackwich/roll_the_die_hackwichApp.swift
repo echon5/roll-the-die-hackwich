0789986296d2be9ee278dc0742d2266311e4d938
Erin Chon <echon@bsd220.org>
2022-11-07 09:26:47 -0600

    Initial Commit

---
+++ roll the die hackwich/roll_the_die_hackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  roll_the_die_hackwichApp.swift
+//  roll the die hackwich
+//
+//  Created by Erin Chon on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct roll_the_die_hackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
